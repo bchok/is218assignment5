2bf1ef1c479927086096f2de64239e38db9a4f82
bchok <bchok91@gmail.com>
2016-11-03 15:31:35 -0400

    added 4 switch statemtns showing use of isset(), empty(), and is_null()

--- is218assignment5.php
+++ is218assignment5.php
@@ -33,6 +33,64 @@ if (isset($working)){
 }else if(is_null($working)){
     echo 'no device status declared<br>';
 }
+/////////////////////////////////////////////////////
+
+switch($car){
+    case isset($car):
+        echo 'you have selected a car type<br>';
+        break;
+    case empty($car):
+        echo 'a car exists but you have not specified what it is<br>';
+        break;
+    case is_null($car):
+        echo 'a car does not exist<br>';
+        break;
+    default:
+        echo 'some form of invalid input was created<br>';
+        break;
+}
+
+switch($bike){
+    case (!isset($bike)):
+        echo 'a bike in some form does not exist<br>';
+        break;
+    case (empty($bike)):
+        echo 'a bike in some form exists<br>';
+        break;
+    case (is_null($bike)):
+        echo 'nothing seems to exist<br>';
+        break;
+    default:
+        echo 'please define some sort of bike<br>';
+        break;
+}
+
+switch($boat){
+    case (isset($boat)):
+        echo 'a boat has been defined<br>';
+        break;
+    case (empty($bike)):
+        echo 'a boat may exist but it may not be defined<br>';
+        break;
+    case (is_null($boat)):
+        echo 'nothing exists or is defined<br>';
+        break;
+    default:
+        echo 'im confused, please create something that is not null<br>';
+        break;
+}
+
+switch($working){
+    case (isset($working) || empty($working)):
+        echo 'your device is working in some state<br>';
+        break;
+    case (is_null($working)):
+        echo 'no status on your device at the moment<br>';
+        break;
+    default:
+        echo 'I am unsure if a device exists<br>';
+        break;
+}
 
 
 
